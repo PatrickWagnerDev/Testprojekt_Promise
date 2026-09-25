bbee052038ec57d2478a26b466b5631078cb0106
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-25 13:59:34 +0200

    add: comments

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let promError = true;
+// Promise
+    // Ein Promise repräsentiert ob etwas abgeschlossen wurde oder ein Fehlschlag ist
+    // In diesem Beispiel, wenn es einen Error gibt (true) dann soll er eine fehlermeldung ausgeben
+        let promError = true;
 
-function getPromise() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (promError) {
-                reject("hat nicht geklappt");
-            } else {
-                resolve("hat geklappt");
-            }
-        }, 300);
-    });
-}
+        function getPromise() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    if (promError) {
+                        reject("hat nicht geklappt");
+                    } else {
+                        resolve("hat geklappt");
+                    }
+                }, 300);
+            });
+        }
 
-function usePromise() {
-    let prom = getPromise();
-    console.log(prom);
-}
+        function usePromise() {
+            let prom = getPromise();
+            console.log(prom);
+        }
