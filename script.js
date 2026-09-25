1238872a266385cd3eda2e6551ee3e17c10483cd
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-25 14:26:32 +0200

    add: try & catch

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 // Promise
     // Ein Promise repräsentiert ob etwas abgeschlossen wurde oder ein Fehlschlag ist
     // In diesem Beispiel, wenn es einen Error gibt (true) dann soll er eine fehlermeldung ausgeben
-        let promError = false;
+        let promError = true;
 
         function getPromise() {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
+                    console.log("1");
                     if (promError) {
                         reject("hat nicht geklappt");
                     } else {
                         resolve("hat geklappt");
                     }
-                }, 2000);
+                }, 300);
             });
         }
 
-        // async sagt, dass in der Funktion ein await ist und das auch dieses gewartet werden soll
-        // bis dieses ausgefüht ist
-        async function usePromise() {
-            console.log("prom");
-            let prom = await getPromise();
-            console.log("prom2");
-            console.log(prom);
+        function getPromise2() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log("2");
+                    if (!promError) {
+                        reject("hat nicht geklappt");
+                    } else {
+                        resolve("hat geklappt");
+                    }
+                }, 300);
+            });
         }
+
+        function getPromise3() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log("3");
+                    if (promError) {
+                        reject("hat nicht geklappt");
+                    } else {
+                        resolve("hat geklappt");
+                    }
+                }, 300);
+            });
+        }
+
+        // Async & await
+            // async sagt, dass in der Funktion ein await ist und das auch dieses gewartet werden soll
+            // bis dieses ausgefüht ist
+        // Try & Catch
+            // Damit wird abgefangen ob etwas geklappt hat und wenn nicht wird etwas anderes ausgeführt
+                async function usePromise() {
+                    try {
+                        await getPromise();
+                        await getPromise2();
+                        await getPromise3();
+                        console.log("juhu");
+                        
+                    } catch (error) {
+                        console.log(error);
+                    }
+                    console.log("Ende");
+                }
